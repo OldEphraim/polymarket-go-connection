a9f433ca87a12d9f9a8ad7909a91d37db502ce0c
Alan Garber <a8garber@yahoo.com>
2025-10-16 00:36:31 -0400

    new endpoint

--- gatherer/gatherer.go
+++ gatherer/gatherer.go
@@ -211,6 +211,24 @@ func (g *Gatherer) startDebugHTTP(addr string) {
 		_ = json.NewEncoder(w).Encode(out)
 	})
 
+	mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		out := struct {
+			AssetsCount int      `json:"assets_count"`
+			Sample      []string `json:"sample"`
+		}{
+			AssetsCount: len(g.wsAssetList()),
+		}
+		// include up to 5 examples to sanity check
+		all := g.wsAssetList()
+		if len(all) > 5 {
+			out.Sample = all[:5]
+		} else {
+			out.Sample = all
+		}
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(out)
+	})
+
 	// Optional: pprof under /debug/pprof/*
 	// The handlers are registered on DefaultServeMux; mount them here.
 	mux.Handle("/debug/pprof/", http.DefaultServeMux)
